38be4202a963f379253f02d6177d70f75b800b52
Wachirawit <ingwachi@gmail.com>
2019-11-23 20:26:43 +0700

    Add user variable

--- src/main/java/th/ac/ku/tutor/model/User.java
+++ src/main/java/th/ac/ku/tutor/model/User.java
@@ -2,14 +2,16 @@ package th.ac.ku.tutor.model;
 
 import lombok.Data;
 import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import javax.validation.constraints.NotNull;
+
 @Data
-@Document
+@Document(collection = "User")
 public class User {
+
     @Id
     private String id;
 
@@ -25,4 +27,20 @@ public class User {
 
     @NonNull
     private String nickname;
+
+    @NotNull
+    private String studentID;
+
+    @NotNull
+    private String campus;
+
+    @NotNull
+    private String faculty;
+
+    @NotNull
+    private String department;
+
+    @NotNull
+    private String kuGen;
+
 }
