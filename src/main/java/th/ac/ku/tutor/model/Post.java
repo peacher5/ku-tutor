206861292c96b2d53e7d0a45302693cfa88d6c5d
Peranut W <peacher5@hotmail.com>
2019-11-25 19:02:42 +0700

    Add Post Model

---
+++ src/main/java/th/ac/ku/tutor/model/Post.java
@@ -0,0 +1,20 @@
+package th.ac.ku.tutor.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document
+public class Post {
+    @Id
+    private String id;
+
+    private String title;
+
+    private String description;
+
+    private String content;
+
+    private String coverPictureUrl;
+}
